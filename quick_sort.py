2733514ba80ddd9e593d655f4a772ae5f6cf6936
abderraouf-metidji <115145685+abderraouf-metidji@users.noreply.github.com>
2023-11-10 10:47:16 +0100

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -5,37 +5,37 @@ Le tri rapide est un algorithme de tri efficace qui fonctionne en choisissant un
 L'algorithme répète ensuite la procédure pour chacun des deux sous-tableaux, jusqu'à ce que tous les éléments du tableau soient triés.
 """
 
-def partition(array, low, high):
+def partition(input_list, low, high):
     
-    pivot = array[high]
+    pivot = input_list[high]
     
     i = low - 1
     
     for j in range(low, high):
-        if array[j] <= pivot:
+        if input_list[j] <= pivot:
             i += 1
-            array[i], array[j] = array[j], array[i]
+            input_list[i], input_list[j] = input_list[j], input_list[i]
             
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    (input_list[i + 1], input_list[high]) = (input_list[high], input_list[i + 1])
     
     return i + 1
 
-def quick_sort(array, low, high):
+def quick_sort(input_list, low, high):
     if low < high:
         
-        pi = partition(array, low, high)
+        pi = partition(input_list, low, high)
         
-        quick_sort(array, low, pi - 1)
+        quick_sort(input_list, low, pi - 1)
         
-        quick_sort(array, pi + 1, high)
+        quick_sort(input_list, pi + 1, high)
         
-array = [64, 34, 25, 12, 22, 11, 90]
-print('Unsorted array')
-print(array)
+input_list = [64, 34, 25, 12, 22, 11, 90]
+print('Unsorted input_list')
+print(input_list)
 
-size = len(array)
+size = len(input_list)
 
-quick_sort(array, 0, size - 1)
+quick_sort(input_list, 0, size - 1)
 
-print('Sorted array:')
-print(array)
+print('Sorted input_list:')
+print(input_list)
